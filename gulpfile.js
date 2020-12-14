65c247404ad8a4f46258e1c66dbcb8f198baa4e8
idawaaler <idax5257@edu.easj.dk>
2020-12-14 21:16:28 +0100

    Nye filer

--- gulpfile.js
+++ gulpfile.js
@@ -7,42 +7,38 @@ const cssnano = require("cssnano");
 const terser = require("gulp-terser");
 const rename = require("gulp-rename");
 
+function styles() {
 
-
-function styles (){
     return(
         gulp.src("css/*.scss")
             .pipe(sourcemaps.init())
             .pipe(sass())
-            //.pipe(postcss([autoprefixer({grid:auto})], cssnano()))
+            .pipe(postcss([autoprefixer({grid:true}), cssnano()]))
             .pipe(sourcemaps.write("."))
             .pipe(gulp.dest("css"))
     );
+
 }
 
 function js() {
     return(
-        gulp.src(["js/*.js","!js/*min.js"])
+        gulp.src(["js/*.js", "!js/*min.js"])
             .pipe(terser())
             .pipe(rename({
-                suffix: ".min"
+                suffix:".min"
             }))
             .pipe(gulp.dest("js"))
-    )
+    );
 }
-
 
 function watch(){
-    gulp.watch("css/*.scss",styles)
-    gulp.watch(["js/*.js","!js/*min.js"],js);
+    gulp.watch("css/*.scss", styles);
+    gulp.watch(["js/*.js", "!js/*min.js"], js);
 }
 
-const build = gulp.parallel(styles,js);
-
+const build = gulp.parallel(styles, js);
 
 exports.styles = styles;
 exports.js = js;
 exports.watch = watch;
 exports.build = build;
-
-
